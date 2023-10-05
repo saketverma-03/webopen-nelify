af67ef9144e0fbf47dab94a1eeb54e9f3a28656c
saket verma <saketverma831@gmail.com>
2023-10-06 02:48:38 +0530

    ADDED: isPublic field to porject schema

--- src/db/models/projectModel.ts
+++ src/db/models/projectModel.ts
@@ -1,44 +1,51 @@
 import mongoose from "mongoose";
+import { Schema, string } from "zod";
 
 export interface IProject {
-  title: string;
-  discription: string;
-  jsCode: string;
-  cssCode: string;
-  htmlCode: string;
-  user: mongoose.Schema.Types.ObjectId;
+    title: string;
+    discription: string;
+    jsCode: string;
+    cssCode: string;
+    htmlCode: string;
+    user: mongoose.Schema.Types.ObjectId;
+    isPublic: boolean
 }
 
 const projectSchema = new mongoose.Schema<IProject>({
-  title: {
-    type: "string",
-    required: true,
-  },
-  discription: {
-    type: "string",
-    required: false,
-  },
-  jsCode: {
-    type: "string",
-    required: false,
-    default: "",
-  },
-  cssCode: {
-    type: "string",
-    required: false,
-    default: "",
-  },
-  htmlCode: {
-    type: "string",
-    required: false,
-    default: "",
-  },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "users",
-  },
+    title: {
+        type: String,
+        required: true,
+    },
+    discription: {
+        type: String,
+        required: false,
+    },
+    jsCode: {
+        type: String,
+        required: false,
+        defaul: "",
+    },
+    cssCode: {
+        type: String,
+        required: false,
+        default: "",
+    },
+    htmlCode: {
+        type: String,
+        required: false,
+        default: "",
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+    },
+    isPublic: {
+        type: Boolean,
+        default: false,
+        required: true
+    }
 });
 
 export const Project =
-  mongoose.models.projects ||
-  mongoose.model<IProject>("projects", projectSchema);
+    mongoose.models.projects ||
+    mongoose.model<IProject>("projects", projectSchema);
