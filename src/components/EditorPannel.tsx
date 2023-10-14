9b024eff24b3ab3ad0b0b3db9dc077bcf99a57f6
saket verma <saketverma831@gmail.com>
2023-10-14 19:46:27 +0530

    ADDED: editor pannel

---
+++ src/components/EditorPannel.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { css } from "@codemirror/lang-css";
+import { html } from "@codemirror/lang-html";
+import { javascript } from "@codemirror/lang-javascript";
+import CodeMirror from "@uiw/react-codemirror";
+
+import { cssCode, htmlCode, jsCode, editorFontSize } from "@/store";
+import { useAtom } from "jotai";
+
+export function JsPannel() {
+  const [code, setCode] = useAtom(jsCode);
+  const [style] = useAtom(editorFontSize);
+
+  return (
+    <CodeMirror
+      value={code}
+      height="100%"
+      theme={"dark"}
+      extensions={[javascript()]}
+      className="editor-view"
+      onChange={(value) => setCode(value)}
+      style={style}
+    />
+  );
+}
+export function HtmlPannel() {
+  const [code, setCode] = useAtom(htmlCode);
+
+  const [style] = useAtom(editorFontSize);
+  return (
+    <CodeMirror
+      value={code}
+      height="100%"
+      theme={"dark"}
+      extensions={[html()]}
+      className="editor-view"
+      onChange={(value) => setCode(value)}
+      style={style}
+    />
+  );
+}
+export function CssPannel() {
+  const [code, setCode] = useAtom(cssCode);
+
+  const [style] = useAtom(editorFontSize);
+  return (
+    <CodeMirror
+      value={code}
+      height="100%"
+      theme={"dark"}
+      extensions={[css()]}
+      className="editor-view"
+      onChange={(value) => setCode(value)}
+      style={style}
+    />
+  );
+}
