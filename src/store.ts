16b76d19f7b29ebeea0e8cf7b418b09ed4fc591f
saket verma <saketverma831@gmail.com>
2023-10-14 19:38:36 +0530

    ADDED: variable for font size

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,15 @@
 import { atom } from "jotai";
+import { ProjUserType } from "./types";
 
 /* GLobal states , similar to redux */
 
+// states for editors
 export const jsCode = atom("");
 export const htmlCode = atom("");
 export const cssCode = atom("");
+export const editorFontSize = atom(
+  {
+    "--e-font-size": "calc(1rem + 0px )",
+  }
+)
+export const projUserType = atom<ProjUserType>("owner")
