4d779a2e8bd2ed98e0f63130ab8bfa306d92e31d
saket verma <saketverma831@gmail.com>
2023-10-14 20:07:29 +0530

    UPDATED: database configuration

--- src/db/dbConfing.ts
+++ src/db/dbConfing.ts
@@ -1,12 +1,10 @@
 import mongoose from "mongoose";
-/* 
+/*
     Creates a instance of the connection object
 */
 export async function dbConnect() {
   try {
-    await mongoose.connect(
-      "mongodb://mongo:1PWm23BqTrSiZBV91PPg@containers-us-west-47.railway.app:7447"
-    );
+    await mongoose.connect(process.env.MONOGO_CONNECT_URL || "");
     const connection = mongoose.connection;
     connection.on("connected", () => console.log("DB connected successfully"));
     connection.on("error", () => {
@@ -14,6 +12,6 @@ export async function dbConnect() {
       process.exit(1);
     });
   } catch (err) {
-    console.log("Something went wrong");
+    console.log("Something went wrong,Could not connect to database");
   }
 }
